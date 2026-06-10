bf4ad7b18b3166c7dcaebbea723b751e138f885d
Eri_k416 <muhammadfachrydzi@gmail.com>
2026-06-10 21:35:17 +0700

    Fixed Vite base value

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/gameified-habit-tracker.git/'
+  base: '/gameified-habit-tracker/'
 })
